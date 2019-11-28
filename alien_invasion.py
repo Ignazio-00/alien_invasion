6675d8f77b392a421d9e5aa2c59f3bdf6fa8fda0
majdukovic <majdukovic@xanadu.ie>
2019-11-28 22:11:46 +0000

    Reset game statistics after each game is over

--- alien_invasion.py
+++ alien_invasion.py
@@ -65,7 +65,17 @@ class AlienInvasion:
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks Play."""
         if self.play_button.rect.collidepoint(mouse_pos):
+            # Reset the game statistics.
+            self.stats.reset_stats()
             self.stats.game_active = True
+
+            # Get rid of any remaining aliens and bullets.
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
