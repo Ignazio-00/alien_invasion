03e9d5ca7f8fb23973a247f265fa9909a2316bdc
majdukovic <majdukovic@xanadu.ie>
2019-11-28 22:01:46 +0000

    Added Play button functionality, when clicked, game starts

--- alien_invasion.py
+++ alien_invasion.py
@@ -54,10 +54,18 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            if event.type == pygame.KEYDOWN:
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+            elif event.type == pygame.KEYDOWN:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when the player clicks Play."""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = True
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
