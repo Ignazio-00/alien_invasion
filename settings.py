a65678877b04d08935e9eb68129422ab689b7d48
majdukovic <majdukovic@xanadu.ie>
2019-11-28 01:02:05 +0000

    Added alien fleet to the screen

--- settings.py
+++ settings.py
@@ -4,15 +4,17 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings
-        self.screen_width = 800
-        self.screen_height = 400
+        self.screen_width = 1200
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 1.5
+        # self.ship_speed = 1.5
+        self.ship_speed = 5.5
 
         # Bullet settings
-        self.bullet_speed = 1.0
+        # self.bullet_speed = 1.0
+        self.bullet_speed = 10.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
